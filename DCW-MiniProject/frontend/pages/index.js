f75049f45f9d735ca1ee3a55bedd6acad7453f59
Wangchaisri <rosebenz2538@gmail.com>
2021-04-21 20:06:24 +0700

    profile

--- DCW-MiniProject/frontend/pages/index.js
+++ DCW-MiniProject/frontend/pages/index.js
@@ -50,7 +50,7 @@ const index = () => {
     </Head>
     <div className={styles.container}><Navbar />
       <div className={styles.title}>
-      <marquee bgcolor="pink" direction="lefe" scrollamount="5" width="100%"><ins>Welcome to Student Data</ins></marquee></div>
+      <marquee bgcolor="pink" direction="lefe" scrollamount="5" width="100%"><ins>Welcome to Thai Dessert by RoseBenz</ins></marquee></div>
       <div className={styles.list}>
         {showStudents()}
       </div>
